e5161213650a5f9dc3e53e290bcf5fccfeb0348c
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2023-01-12 20:56:10 -0300

    feat: added news tests in test_account.py

--- backend/accounts/tests/test_account.py
+++ backend/accounts/tests/test_account.py
@@ -1,7 +1,7 @@
 import pytest
 
-
 pytestmark = pytest.mark.django_db
+
 
 def test_create_user(customuser):
     user = customuser.objects.create(username='will', email='will@email.com', password='testpass123')
@@ -12,3 +12,13 @@ def test_create_user(customuser):
     assert user.is_staff == False
     assert user.is_superuser == False
     assert customuser.objects.all().count() == 1
+
+
+def test_create_superuser(customuser):
+    user = customuser.objects.create_superuser(username='superwill', email='superwill@email.com',
+                                               password='testpass123')
+    assert user.username == 'superwill'
+    assert user.email == 'superwill@email.com'
+    assert user.is_active
+    assert user.is_staff
+    assert user.is_superuser
