0278d9d3104d888079961e7790e438862356c99b
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2023-01-13 14:22:33 -0300

    feat: improvement in tests in core app

--- backend/core/tests/conftest.py
+++ backend/core/tests/conftest.py
@@ -1,5 +1,4 @@
 import pytest
-# from time import
 from ..models import Patiente
 
 
--- backend/core/tests/test_core_model.py
+++ backend/core/tests/test_core_model.py
@@ -4,9 +4,9 @@ import pytest
 pytestmark = pytest.mark.django_db
 
 
-def test_patiente_info(setupaciente):
-    assert setupaciente.name == 'will smith'
-    assert setupaciente.cpf == '09632222145'
-    assert setupaciente.cel == '21963256987'
-    assert setupaciente.address == 'street view'
+# def test_patiente_info(setupaciente):
+#     assert setupaciente.name == 'will smith'
+#     assert setupaciente.cpf == '09632222145'
+#     assert setupaciente.cel == '21963256987'
+#     assert setupaciente.address == 'street view'
 
