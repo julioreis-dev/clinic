a0faf5605e93eb27bb2fbd8ae0e4f936dc437226
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2023-01-13 14:14:50 -0300

    feat: added news tests to core app

---
+++ backend/core/tests/conftest.py
@@ -0,0 +1,8 @@
+import pytest
+# from time import
+from ..models import Patiente
+
+
+@pytest.fixture(scope='function')
+def setupaciente():
+    return Patiente.objects.create(name='will smith', cpf='09632222145', cel='21963256987', address='street view')
---
+++ backend/core/tests/test_core_model.py
@@ -0,0 +1,12 @@
+import pytest
+
+
+pytestmark = pytest.mark.django_db
+
+
+def test_patiente_info(setupaciente):
+    assert setupaciente.name == 'will smith'
+    assert setupaciente.cpf == '09632222145'
+    assert setupaciente.cel == '21963256987'
+    assert setupaciente.address == 'street view'
+
