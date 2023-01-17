4ffbcf3c7af24016c6c50c0db8265b0117ae819e
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2023-01-17 11:28:51 -0300

    refactor: the files url.py in core app and clinicAdmin project were refactored

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -5,7 +5,7 @@ from .views import PatienteViewSet
 app_name = 'core_api'
 
 router = DefaultRouter()
-router.register('patiente', viewset= PatienteViewSet, basename='patienteviewset')
+router.register('patiente', viewset=PatienteViewSet, basename='patienteviewset')
 
 urlpatterns = [
     path('', include(router.urls)),
