3304e43e6017996da5311fc1fd2a379a9a31a3af
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2023-01-17 22:52:33 -0300

    feat: The app recipes was created and to each app was created a model directory

--- backend/clinicAdmin/settings/settings.py
+++ backend/clinicAdmin/settings/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_filters',
     'accounts',
     'core',
+    'recipes',
 ]
 
 MIDDLEWARE = [
---
+++ backend/recipes/models/Prescription.py
@@ -0,0 +1,26 @@
+from ..models import *
+from core.models import Patiente
+
+
+class Base(models.Model):
+    """
+    Abstract Class used in other classes
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        """
+        This class makes the class Base be abstract
+        """
+        abstract = True
+
+
+class Prescription(Base):
+    observations = models.TextField(verbose_name='Observações', null=True, blank=True)
+    medication = models.TextField(verbose_name='Medicação', null=True, blank=True)
+    exams = models.TextField(verbose_name='Exames', null=True, blank=True)
+    patiente = models.ForeignKey(Patiente, null=True, blank=True, on_delete=models.CASCADE, related_name='prescription')
+
+    class Meta:
+        app_label = 'recipes'
