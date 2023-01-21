2f70c8c6e81ba87650586377738ec88b18cad18b
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2023-01-20 22:08:36 -0300

    test: created tests to recipes app

--- backend/recipes/tests/test_recipes_model.py
+++ backend/recipes/tests/test_recipes_model.py
@@ -1,3 +1,8 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+pytestmark = pytest.mark.django_db
+
+def test_prescrition_info(setprescrition):
+    assert setprescrition[0].alegations == setprescrition[1]['alegations']
+    assert setprescrition[0].medication == setprescrition[1]['medication']
+    assert setprescrition[0].exams == setprescrition[1]['exams']
