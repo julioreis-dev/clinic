e2489198a8f76bd59c56c61037248134a3d4874f
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2023-01-20 22:56:56 -0300

    test: improvements in tests inside core app

--- backend/core/tests/conftest.py
+++ backend/core/tests/conftest.py
@@ -1,7 +1,24 @@
 import pytest
 from ..models import Patiente
+from faker import Faker
 
 
 @pytest.fixture(scope='function')
 def setupaciente():
-    return Patiente.objects.create(name='will smith', cpf='09632222145', birth='2023-02-18', cel='21963256987', address='street view')
+    faker = Faker()
+    dict_faker = {'name': faker.name(),
+                  'cpf': faker.random_int(11111111111, 99999999999),
+                  'birth': faker.date_of_birth(),
+                  'cel': faker.random_int(11111111111, 99999999999),
+                  'address': faker.address(),
+                  'profession': faker.job(),
+                  'active': False,
+                  'gender': faker.profile()['sex']}
+    return Patiente.objects.create(name=dict_faker['name'],
+                                   cpf=dict_faker['cpf'],
+                                   birth=dict_faker['birth'],
+                                   cel=dict_faker['cel'],
+                                   address=dict_faker['address'],
+                                   profession=dict_faker['profession'],
+                                   active=dict_faker['active'],
+                                   gender=dict_faker['gender']), dict_faker
--- backend/core/tests/test_core_model.py
+++ backend/core/tests/test_core_model.py
@@ -1,13 +1,14 @@
 import pytest
 
-
 pytestmark = pytest.mark.django_db
 
 
 def test_patiente_info(setupaciente):
-    assert setupaciente.name == 'will smith'
-    assert setupaciente.cpf == '09632222145'
-    assert setupaciente.cel == '21963256987'
-    assert setupaciente.birth =='2023-02-18'
-    assert setupaciente.address == 'street view'
-
+    assert setupaciente[0].name == setupaciente[1]['name']
+    assert setupaciente[0].cpf == setupaciente[1]['cpf']
+    assert setupaciente[0].cel == setupaciente[1]['cel']
+    assert setupaciente[0].birth == setupaciente[1]['birth']
+    assert setupaciente[0].address == setupaciente[1]['address']
+    assert setupaciente[0].profession == setupaciente[1]['profession']
+    assert setupaciente[0].active == setupaciente[1]['active']
+    assert setupaciente[0].gender == setupaciente[1]['gender']
