8aa38c96acf7dc855812434187b599ed0c292209
Julio dos Reis Firmino <julreifir@yahoo.com.br>
2023-01-28 22:02:31 -0300

    feat: created a view to route

--- backend/core/views.py
+++ backend/core/views.py
@@ -2,9 +2,30 @@ from rest_framework import viewsets, status
 from rest_framework.views import APIView
 from .models import Patiente
 from .serializers import PatienteSerializers
+from rest_framework import mixins
 from rest_framework.response import Response
+from rest_framework.decorators import action
+from historical.serializers import HistorySerializers
 
-
-class PatienteViewSet(viewsets.ReadOnlyModelViewSet):
+class PatientesViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    All patientes in database
+    """
     serializer_class = PatienteSerializers
     queryset = Patiente.objects.all()
+
+    @action(detail=True, methods=['get'])
+    def listhistory(self, request, pk=None):
+        """
+        History of a patiente
+        """
+        response = self.get_object()
+        serializer = HistorySerializers(response.history, many=False)
+        return Response(serializer.data)
+
+#
+#
+#
+#
+# class PatienteViewSet(viewsets.ModelViewSet):
+#     pass
